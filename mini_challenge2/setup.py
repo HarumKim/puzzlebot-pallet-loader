60b50b6072b3f749a515ad2f64af06c5ff8ec2c7
HarumKim <harumkim09@gmail.com>
2026-04-20 12:03:42 -0600

    Initial Commit

---
+++ mini_challenge2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+package_name = 'mini_challenge2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.[yma]*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
+        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*.stl'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Harum Kim',
+    maintainer_email='harumkim09@gmail.com',
+    description='Challenge Week 1',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'circular_movement = mini_challenge2.circular_movement:main'
+        ],
+    },
+)
